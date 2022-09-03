1951628d01b0cba14a83537272e0bcf72cb16140
Shahab <shahab.molouk@gmail.com>
2022-09-03 08:44:09 +0200

    Created

---
+++ CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using CarInsurance.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+        // GET: Insuree
+      
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+       
+
+
+    }
+}
---
+++ CarInsurance/Controllers/InsureeController.cs
@@ -0,0 +1,159 @@
+﻿using CarInsurance.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                insuree.Quote = CalculateQuote(insuree);
+                db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                insuree.Quote = CalculateQuote(insuree);
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public decimal CalculateQuote(Insuree insuree)
+        {
+            //Start with a base of $50 / month.
+            decimal quote = 50.0M;
+            //If the user is 18 and under, add $100 to the monthly total.
+            if (insuree.DateOfBirth > DateTime.Now.AddYears(-18)) quote += 100;
+            //If the user is between 19 and 25, add $50 to the monthly total.
+            else if (insuree.DateOfBirth >= DateTime.Now.AddYears(-25)) quote += 50;
+            //If the user is over 25, add $25 to the monthly total.
+            else quote += 25;
+
+            //If the car's year is before 2000, add $25 to the monthly total.
+            //If the car's year is after 2015, add $25 to the monthly total.
+            if (insuree.CarYear < 2000 || insuree.CarYear > 2015) quote += 25;
+
+            //If the car's Make is a Porsche, add $25 to the price.
+            if (insuree.CarMake == "Porsche") quote += 25;
+            
+            //If the car's Make is a Porsche and its model is a 911 Carrera, add an additional $25 to the price.
+            if (insuree.CarMake == "Porsche" && insuree.CarModel=="911") quote += 25;
+
+            //Add $10 to the monthly total for every speeding ticket the user has.
+            quote += 10 * insuree.SpeedingTickets;
+
+            //If the user has ever had a DUI, add 25 % to the total.
+            if (insuree.DUI) quote = quote * 1.25M;
+
+            //If it's full coverage, add 50% to the total.
+            if (insuree.CoverageType) quote *= 1.5M;
+
+            return quote;
+        }
+    }
+}
