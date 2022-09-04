5e84ed4643dbdebfacae1bfbbdc7e77ef365f58b
Shahab <shahab.molouk@gmail.com>
2022-09-04 12:20:47 +0200

    Create Project

---
+++ CodeFirstSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.firstName = "Howard";
+            student1.lastName = "Watson";
+            PersonContext ctxt = new PersonContext();
+            ctxt.Students.Add(student1);
+            ctxt.SaveChanges();
+
+            Console.WriteLine("Finished");
+            Console.ReadKey();
+    }
+    }
+}
---
+++ CodeFirstSample/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstSample
+{
+    public class Student
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+
+
+    public class PersonContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
